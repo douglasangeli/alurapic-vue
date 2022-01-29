dba99734cd028d30370115b6d6998d57ece7fce9
Douglas Angeli <douglasangeli92@gmail.com>
2022-01-29 12:27:40 -0300

    2.7 importando o bootstrap

--- app/src/main.js
+++ app/src/main.js
@@ -8,6 +8,8 @@ import { routes } from "./routes";
 import "./directives/Transform";
 import msg from "./pt_BR";
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 Vue.use(VueResource);
 Vue.http.options.root = "http://localhost:3000";
 Vue.use(VueRouter);
