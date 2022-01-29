061d4441fa9188ae41879a353316f6413ba7df63
Douglas Angeli <douglasangeli92@gmail.com>
2022-01-29 11:25:20 -0300

    2.5 validação de formulário

--- app/src/components/cadastro/Cadastro.vue
+++ app/src/components/cadastro/Cadastro.vue
@@ -9,12 +9,34 @@
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo" />
+        <input
+          name="titulo"
+          v-validate
+          data-vv-rules="required|min:3|max:30"
+          data-vv-as="título"
+          id="titulo"
+          autocomplete="off"
+          v-model="foto.titulo"
+        />
+        <span class="erro" v-show="errors.has('titulo')">
+          {{ errors.first("titulo") }}
+        </span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url" />
+        <input
+          name="url"
+          v-validate
+          data-vv-rules="required"
+          data-vv-as="URL"
+          id="url"
+          autocomplete="off"
+          v-model="foto.url"
+        />
+        <span class="erro" v-show="errors.has('url')">
+          {{ errors.first("url") }}
+        </span>
         <imagem-responsiva
           v-show="foto.url"
           :url="foto.url"
@@ -60,13 +82,17 @@ export default {
   },
   methods: {
     grava() {
-      this.service.cadastra(this.foto).then(
-        () => {
-          if (this.id) this.$router.push({ name: "home" });
-          this.foto = new Foto();
-        },
-        (err) => console.log(err)
-      );
+      this.$validator.validateAll().then((success) => {
+        if (success) {
+          this.service.cadastra(this.foto).then(
+            () => {
+              if (this.id) this.$router.push({ name: "home" });
+              this.foto = new Foto();
+            },
+            (err) => console.log(err)
+          );
+        }
+      });
     },
   },
   created() {
@@ -99,5 +125,10 @@ export default {
 
 .centralizado {
   text-align: center;
+}
+
+.erro {
+  color: red;
+  font-size: small;
 }
 </style>
--- app/src/main.js
+++ app/src/main.js
@@ -3,12 +3,19 @@ import App from "./App.vue";
 
 import VueResource from "vue-resource";
 import VueRouter from "vue-router";
+import VeeValidate from "vee-validate";
 import { routes } from "./routes";
 import "./directives/Transform";
+import msg from "./pt_BR";
 
 Vue.use(VueResource);
-Vue.http.options.root = "http://localhost:3000"
+Vue.http.options.root = "http://localhost:3000";
 Vue.use(VueRouter);
+
+Vue.use(VeeValidate, {
+  locale: "pt_BR",
+  dictionary: { pt_BR: msg }
+});
 
 const router = new VueRouter({
   routes,
